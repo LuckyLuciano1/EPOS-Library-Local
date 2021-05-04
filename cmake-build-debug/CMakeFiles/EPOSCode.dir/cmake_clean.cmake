c5a87484111ad2f1140ea11fa3102fe64a18aeec
LuckyLuciano1 <lucasutke@gmail.com>
2021-05-04 09:12:10 -0600

    5.4.2021

---
+++ cmake-build-debug/CMakeFiles/EPOSCode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EPOSCode.dir/main.cpp.obj"
+  "EPOSCode.exe"
+  "EPOSCode.exe.manifest"
+  "EPOSCode.pdb"
+  "libEPOSCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EPOSCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/EPOSCode.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EPOSCode.
+# This may be replaced when dependencies are built.
